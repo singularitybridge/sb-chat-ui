5b5402ea8d0110e353878905fa40d527420763b8
avi o <avi.o@circlesip.com>
2023-01-22 09:01:37 +0200

    Added ai therapists imageS

--- src/pages/ChatBots.js
+++ src/pages/ChatBots.js
@@ -1,31 +1,44 @@
-import React, { useEffect, useState } from "react";
-import styled from "styled-components";
-import tw from "twin.macro";
-import { ReactComponent as SvgDotPatternIcon } from "../images/dot-pattern.svg";
-import { SectionHeading as HeadingTitle } from "../components/misc/Headings.js";
-import { css } from "styled-components/macro"; //eslint-disable-line
-import { slide as Menu } from "react-burger-menu";
-import TopBar from "components/headers/topBar";
-import axios from "axios";
-import { ReactComponent as StarIcon } from "images/star-icon.svg";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
+import tw from "twin.macro";
+import styled from "styled-components";
+import { css } from "styled-components/macro"; //eslint-disable-line
+import { Container, ContentWithPaddingXl } from "components/misc/Layouts.js";
+import { SectionHeading } from "components/misc/Headings.js";
 import { PrimaryButton as PrimaryButtonBase } from "components/misc/Buttons.js";
+import { ReactComponent as StarIcon } from "images/star-icon.svg";
+import { ReactComponent as SvgDecoratorBlob1 } from "images/svg-decorator-blob-5.svg";
+import { ReactComponent as SvgDecoratorBlob2 } from "images/svg-decorator-blob-7.svg";
 
-const Container = tw.div`relative`;
 
-const SingleColumn = tw.div`max-w-screen-xl mx-auto py-20 lg:py-24`;
+import av1 from "images/therapist-avatars/av1.png";
+import av2 from "images/therapist-avatars/av2.png";
+import av3 from "images/therapist-avatars/av3.png";
+import av4 from "images/therapist-avatars/av4.png";
+import av5 from "images/therapist-avatars/av5.png";
+import av6 from "images/therapist-avatars/av6.png";
+import av7 from "images/therapist-avatars/av7.png";
+import av8 from "images/therapist-avatars/av8.png";
+import av9 from "images/therapist-avatars/av9.png";
 
-const HeadingInfoContainer = tw.div`flex flex-col items-center`;
-const HeadingDescription = tw.p`mt-4 font-medium text-gray-600 text-center max-w-sm`;
+const HeaderRow = tw.div`flex justify-between items-center flex-col xl:flex-row`;
+const Header = tw(SectionHeading)``;
+const TabsControl = tw.div`flex flex-wrap bg-gray-200 px-2 py-2 rounded leading-none mt-12 xl:mt-0`;
 
-const Content = tw.div`mt-16`;
+const TabControl = styled.div`
+  ${tw`cursor-pointer px-6 py-3 mt-2 sm:mt-0 sm:mr-2 last:mr-0 text-gray-600 font-medium rounded-sm transition duration-300 text-sm sm:text-base w-1/2 sm:w-auto text-center`}
+  &:hover {
+    ${tw`bg-gray-300 text-gray-700`}
+  }
+  ${props => props.active && tw`bg-primary-500! text-gray-100!`}
+  }
+`;
 
+const TabContent = tw(motion.div)`mt-6 flex flex-wrap sm:-mr-10 md:-mr-6 lg:-mr-12`;
 const CardContainer = tw.div`mt-10 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 sm:pr-10 md:pr-6 lg:pr-12`;
+const Card = tw(motion.a)`bg-gray-200 rounded-b block max-w-xs mx-auto sm:max-w-none sm:mx-0`;
 const CardImageContainer = styled.div`
-  ${(props) =>
-    css`
-      background-image: url("${props.imageSrc}");
-    `}
+  ${props => css`background-image: url("${props.imageSrc}");`}
   ${tw`h-56 xl:h-64 bg-center bg-cover relative rounded-t`}
 `;
 const CardRatingContainer = tw.div`leading-none absolute inline-flex bg-gray-100 bottom-0 left-0 ml-4 mb-4 rounded-full px-5 py-2 items-end`;
@@ -49,50 +62,125 @@ const CardTitle = tw.h5`text-lg font-semibold group-hover:text-primary-500`;
 const CardContent = tw.p`mt-1 text-sm font-medium text-gray-600`;
 const CardPrice = tw.p`mt-4 text-xl font-bold`;
 
-const Card = tw(
-  motion.a
-)`bg-gray-200 rounded-b block max-w-xs mx-auto sm:max-w-none sm:mx-0`;
-const Image = styled.div((props) => [
-  `background-image: url("${props.imageSrc}");`,
-  tw`rounded md:w-1/2 lg:w-5/12 xl:w-1/3 flex-shrink-0 h-80 md:h-144 bg-cover bg-center mx-4 sm:mx-8 md:mx-4 lg:mx-8`,
-]);
-const Details = tw.div`mt-4 md:mt-0 md:max-w-md mx-4 sm:mx-8 md:mx-4 lg:mx-8`;
-const Subtitle = tw.div`font-bold tracking-wide text-secondary-100`;
-const Title = tw.h4`text-3xl font-bold text-gray-900`;
-const Description = tw.p`mt-2 text-sm leading-loose`;
-const Link = tw.a`inline-block mt-4 text-sm text-primary-500 font-bold cursor-pointer transition duration-300 border-b-2 border-transparent hover:border-primary-500`;
+const DecoratorBlob1 = styled(SvgDecoratorBlob1)`
+  ${tw`pointer-events-none -z-20 absolute right-0 top-0 h-64 w-64 opacity-15 transform translate-x-2/3 -translate-y-12 text-pink-400`}
+`;
+const DecoratorBlob2 = styled(SvgDecoratorBlob2)`
+  ${tw`pointer-events-none -z-20 absolute left-0 bottom-0 h-80 w-80 opacity-15 transform -translate-x-2/3 text-primary-500`}
+`;
 
-const SvgDotPattern1 = tw(
-  SvgDotPatternIcon
-)`absolute top-0 left-0 transform -translate-x-20 rotate-90 translate-y-8 -z-10 opacity-25 text-primary-500 fill-current w-24`;
-const SvgDotPattern2 = tw(
-  SvgDotPatternIcon
-)`absolute top-0 right-0 transform translate-x-20 rotate-45 translate-y-24 -z-10 opacity-25 text-primary-500 fill-current w-24`;
-const SvgDotPattern3 = tw(
-  SvgDotPatternIcon
-)`absolute bottom-0 left-0 transform -translate-x-20 rotate-45 -translate-y-8 -z-10 opacity-25 text-primary-500 fill-current w-24`;
-const SvgDotPattern4 = tw(
-  SvgDotPatternIcon
-)`absolute bottom-0 right-0 transform translate-x-20 rotate-90 -translate-y-24 -z-10 opacity-25 text-primary-500 fill-current w-24`;
-
-export default () => {
- 
-  const [stories, setStories] = useState([]);
-
-  useEffect(() => {
-    axios({
-      method: "GET",
-      url: "https://api.baserow.io/api/database/rows/table/132652/?user_field_names=true",
-      headers: {
-        Authorization: "token xYNbZuE6CN4KarOStEiUnMqQTU920uw6",
+export default ({
+  heading = "Meet out AI Therapists",
+  tabs = {
+    Starters: [
+      {
+        imageSrc: av1,    
+        title: "Dr. Empathy",
+        content:"Specializes in helping parents understand and empathize with their child's perspective",    
       },
-    }).then((response) => {
-      console.log("data back");
-      console.log(response.data.results);
-      setStories(response.data.results);
-    });
-  }, []);
+      {
+        imageSrc: av2,    
+        title: "Mr. Consistency",
+        content:"Specializes in helping parents set and enforce consistent boundaries and rules",    
+      },
+      {
+        imageSrc: av3,    
+        title: "Ms. Communication",
+        content:"Specializes in helping parents improve communication with their child.",    
+      },
+      {
+        imageSrc: av4,    
+        title: "Dr. Positive Reinforcement",
+        content:"Specializes in helping parents implement positive reinforcement strategies to encourage good behavior",    
+      },
+      {
+        imageSrc: av5,    
+        title: "Mr. Behavioral Challenge",
+        content:"Specializes in helping parents manage behavioral challenges such as tantrums or self-injurious behavior.",    
+      },
+      {
+        imageSrc: av6,   
+        title: "Ms. Self-Regulation",
+        content:"Specializes in helping parents and children develop self-regulation skills, such as sleep and stress management.",    
+      },
+    ],
+    Main: getRandomCards(),
+    Soup: getRandomCards(),
+    Desserts: getRandomCards()
+  }
+}) => {
+  /*
+   * To customize the tabs, pass in data using the `tabs` prop. It should be an object which contains the name of the tab
+   * as the key and value of the key will be its content (as an array of objects).
+   * To see what attributes are configurable of each object inside this array see the example above for "Starters".
+   */
+  const tabsKeys = Object.keys(tabs);
+  const [activeTab, setActiveTab] = useState(tabsKeys[0]);
 
+  return (
+    <Container>
+      <ContentWithPaddingXl>
+        <HeaderRow>
+          <Header>{heading}</Header>
+        </HeaderRow>
+
+        {tabsKeys.map((tabKey, index) => (
+          <TabContent
+            key={index}
+            variants={{
+              current: {
+                opacity: 1,
+                scale:1,
+                display: "flex",
+              },
+              hidden: {
+                opacity: 0,
+                scale:0.8,
+                display: "none",
+              }
+            }}
+            transition={{ duration: 0.4 }}
+            initial={activeTab === tabKey ? "current" : "hidden"}
+            animate={activeTab === tabKey ? "current" : "hidden"}
+          >
+            {tabs[tabKey].map((card, index) => (
+              <CardContainer key={index}>
+                <Card className="group" href={card.url} initial="rest" whileHover="hover" animate="rest">
+                  <CardImageContainer imageSrc={card.imageSrc}>
+                    <CardHoverOverlay
+                      variants={{
+                        hover: {
+                          opacity: 1,
+                          height: "auto"
+                        },
+                        rest: {
+                          opacity: 0,
+                          height: 0
+                        }
+                      }}
+                      transition={{ duration: 0.3 }}
+                    >
+                      <CardButton>Chat</CardButton>
+                    </CardHoverOverlay>
+                  </CardImageContainer>
+                  <CardText>
+                    <CardTitle>{card.title}</CardTitle>
+                    <CardContent>{card.content}</CardContent>
+                  </CardText>
+                </Card>
+              </CardContainer>
+            ))}
+          </TabContent>
+        ))}
+      </ContentWithPaddingXl>
+      <DecoratorBlob1 />
+      <DecoratorBlob2 />
+    </Container>
+  );
+};
+
+/* This function is only there for demo purposes. It populates placeholder cards */
+const getRandomCards = () => {
   const cards = [
     {
       imageSrc: "https://images.unsplash.com/photo-1550699026-4114bbf4fb49?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=632&q=80",    
@@ -124,59 +212,8 @@ export default () => {
       title: "Ms. Self-Regulation",
       content:"Specializes in helping parents and children develop self-regulation skills, such as sleep and stress management.",    
     },
-  ];
+  ]
 
-  return (
-    <Container>
-      {/* <TopBar /> */}
-      <SingleColumn>
-        <HeadingInfoContainer>
-          <HeadingTitle>Chat Bots</HeadingTitle>
-          <HeadingDescription>
-            Meet our team, they are excited to serve you.
-          </HeadingDescription>
-        </HeadingInfoContainer>
-
-        <Content>
-          {cards.map((card, index) => (
-          <CardContainer key={"s1"}>
-            <Card
-              className="group"
-              href={card.url}
-              initial="rest"
-              whileHover="hover"
-              animate="rest"
-            >
-              <CardImageContainer imageSrc={card.imageSrc}>
-                <CardHoverOverlay
-                  variants={{
-                    hover: {
-                      opacity: 1,
-                      height: "auto",
-                    },
-                    rest: {
-                      opacity: 0,
-                      height: 0,
-                    },
-                  }}
-                  transition={{ duration: 0.3 }}
-                >
-                  <CardButton>Chat</CardButton>
-                </CardHoverOverlay>
-              </CardImageContainer>
-              <CardText>
-                <CardTitle>{card.title}</CardTitle>
-                <CardContent>{card.content}</CardContent>
-              </CardText>
-            </Card>
-          </CardContainer>
-          ))}
-        </Content>
-      </SingleColumn>
-      <SvgDotPattern1 />
-      <SvgDotPattern2 />
-      <SvgDotPattern3 />
-      <SvgDotPattern4 />
-    </Container>
-  );
+  // Shuffle array
+  return cards.sort(() => Math.random() - 0.5);
 };
