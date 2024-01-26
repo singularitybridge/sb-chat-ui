224ee32cab19b49aa434d4f8a23a1f176cf776cc
Avi.O <aviosipov@gmail.com>
2024-01-26 19:41:50 +0200

    Update TagInput component with new icons and styling

--- src/components/TagInput.tsx
+++ src/components/TagInput.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { IconButton } from './admin/IconButton';
-import { XMarkIcon } from '@heroicons/react/24/solid';
+import { BeakerIcon, XMarkIcon } from '@heroicons/react/24/solid';
 
 interface TagInputProps {
   title: string;
@@ -9,10 +9,14 @@ interface TagInputProps {
 
 const TagInput: React.FC<TagInputProps> = ({ title, onRemove }) => {
   return (
-    <span className="tag inline-flex items-center bg-violet-100  text-slate-700 text-sm font-normal  mr-2.5 px-3 py-1.5 rounded-xl">
+    <span className="tag inline-flex items-center bg-primary-100  text-slate-700  mr-2.5 px-3 py-2 rounded-2xl">
       {title}
       <IconButton
-        icon={<XMarkIcon className="ml-3 w-3 h-3 text-stone-900" />}
+        icon={<XMarkIcon className="ml-5 w-4 h-4 text-stone-900" />}
+        onClick={() => onRemove(title)}
+      />
+      <IconButton
+        icon={<BeakerIcon className="ml-1 w-4 h-4 text-stone-900" />}
         onClick={() => onRemove(title)}
       />
     </span>
