8281015c0e631994a4e06189d2db0c607a4b50ed
rivkyschon <rivkyschon@outlook.co.il>
2024-06-30 12:24:34 +0300

    add http request log

--- src/services/AxiosService.ts
+++ src/services/AxiosService.ts
@@ -1,17 +1,18 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL:  import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 // Request interceptor to include the Bearer token
 apiClient.interceptors.request.use(
   async config => {
-    const token = localStorage.getItem('userToken'); 
-    
+    const token = localStorage.getItem('userToken');
+
     if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
+      config.headers.Authorization = `Bearer ${token}`;
     }
+    console.log('request URL:  ' + config.baseURL + config.url);
     return config;
   },
   error => {
