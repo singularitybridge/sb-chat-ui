34cb81e95cf3fb6814b9b79d3de129a0995c66d8
avi.o <aviosipov@gmail.com>
2025-06-23 18:10:03 +0300

    chore: Update Vite configuration for improved server and build settings

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,11 @@ export default defineConfig({
       '/api': 'http://localhost:5000',
     },
   },
+  preview: {
+    port: process.env.PORT ? parseInt(process.env.PORT, 10) : 8080,
+    strictPort: true,
+    host: true,
+  },
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1000, // Increase the chunk size warning limit to 1MB (adjust as needed)
